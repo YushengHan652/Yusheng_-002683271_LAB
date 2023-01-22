4de9932e0d214b777ac3a252933908236059b817
Sean Han <seanmacbook@SeanMacbook.local>
2023-01-22 10:56:52 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author seanmacbook
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        int n = 2023;
+        double number= 1.21;
+        float floatNumber = -32.4f;
+        String massage = "Happy new year";
+        boolean flag = true;
+        char letter = '\u0025'; 
+        int[] age = new int[n];
+        System.out.println(n); 
+        System.out.println(number); 
+        System.out.println(floatNumber); 
+        System.out.println(massage); 
+        System.out.println(flag); 
+        System.out.println(letter); 
+  }
+}
